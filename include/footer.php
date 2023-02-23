60e1389606d756d34fddf2aefb9b9d2c3215a979
navika <rachitnavika1104@gmail.com>
2023-02-23 22:50:17 +0530

    added file

---
+++ include/footer.php
@@ -0,0 +1,84 @@
+<footer id="footer" class="footer">
+    <div class="footer-widget-area">
+      <div class="container pt-90 pb-60">
+        <div class="row">
+          <div class="col-md-6 col-lg-6 col-xl-3">
+            <img class="mb-30" alt="Logo" width="180" src="images/logo.png">
+            <p>True Law Association is well-positioned to meet the legal needs of dynamic businesses and the individuals who own and operate them.</p>
+          </div>
+          <div class="col-md-6 col-lg-6 col-xl-3">
+            <div class="widget widget_nav_menu split-nav-menu clearfix">
+              <h4 class="widget-title widget-title-line-bottom line-bottom-theme-colored1">Services</h4>
+              <ul>
+                <li><a href="about.php">About Us</a></li>
+                <!-- <li><a href="index-mp-layout1.html">Services</a></li>
+                <li><a href="index-mp-layout1.html">Attorneys</a></li> -->
+                <li><a href="index-mp-layout1.html">Practice Arears</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <!-- <li><a href="index-mp-layout1.html">Services</a></li> -->
+                <!-- <li><a href="index-mp-layout1.html">Attorneys</a></li> -->
+                <!-- <li><a href="index-mp-layout1.html">Blog</a></li> -->
+               
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-6 col-xl-3">
+            <!-- <div class="widget">
+              <h4 class="widget-title widget-title-line-bottom line-bottom-theme-colored1">Latest News</h4>
+              <div class="latest-posts">
+                <article class="post clearfix pb-0 mb-20">
+                  <a class="post-thumb" href="news-details.html"><img src="images/blog/s1.jpg" alt=""></a>
+                  <div class="post-right">
+                    <span class="post-date">
+                      <time class="entry-date text-theme-colored1" datetime="2020-05-15T06:10:26+00:00">April 15, 2020</time>
+                    </span>
+                    <h6 class="post-title mt-10"> <a href="news-details.html">We’re Providing the Quality Services</a> </h6>
+                  </div>
+                </article>
+                <article class="post clearfix pb-0 mb-20">
+                  <a class="post-thumb" href="news-details.html"><img src="images/blog/s2.jpg" alt=""></a>
+                  <div class="post-right">
+                    <span class="post-date">
+                      <time class="entry-date text-theme-colored1" datetime="2020-05-15T06:10:26+00:00">April 15, 2020</time>
+                    </span>
+                    <h6 class="post-title mt-10"> <a href="news-details.html">We’re Providing the Quality Services</a> </h6>
+                  </div>
+                </article>
+              </div>
+            </div> -->
+          </div>
+          <div class="col-md-6 col-lg-6 col-xl-3 p-0">
+            <div class="widget p-40" data-tm-bg-color="#192637">
+              <h4 class="widget-title">Contact</h4>
+              <ul class="tm-widget">
+                <li><span class="fa fa-phone-square text-theme-colored1 mr-10"></span> <span> <a href="tel:+91-7065030001">+91-7065030001</a><br><span class="fa fa-phone-square text-theme-colored1 mr-10"></span><a href="tel:+91-8826030001">+91-8826030001</a></span></li>
+                <li><span class="fa fa-envelope text-theme-colored1 mr-10"></span> <span style="font-size: 16px"> <a href="mailto:info@truelaw.com">vanshitajain@gmail.com</a></span></li>
+                <li><span class="fa fa-map-marker text-theme-colored1 mr-10"></span> <span style="font-size: 14px">LAWYERS CHAMBER<br> F-611, 6TH FLOOR KARKARDOOMA COURTS<br> DELHI – 110032</span></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom" data-tm-bg-color="#172736">
+        <div class="container">
+          <div class="row pt-20 pb-20">
+            <div class="col-sm-6">
+              <div class="footer-paragraph text-center text-xl-left text-lg-start text-md-left mb-sm-30">
+                © 2021 True Law Assiociation. All Rights Reserved. Designed by <a href="https://www.saavytechnologies.com">Saavy Technologies</a>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="footer-paragraph text-center text-xl-end text-lg-end text-md-end">
+                <ul class="styled-icons icon-md icon-dark icon-hover-theme-colored1 icon-circled clearfix">
+                  <li><a class="social-link" href="https://www.facebook.com/tomar.lawyers"><i class="fab fa-facebook"></i></a></li>
+                  <li><a class="social-link" href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a class="social-link" href="#"><i class="fab fa-youtube"></i></a></li>
+                  <li><a class="social-link" href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
